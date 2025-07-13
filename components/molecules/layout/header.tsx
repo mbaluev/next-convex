f643c00df0f582b397b880f9dee4d7c7fd49a981
mbaluev <mikhail.baluev@gmail.com>
2025-07-13 18:43:40 +0200

    transitions

--- components/molecules/layout/header.tsx
+++ components/molecules/layout/header.tsx
@@ -72,7 +72,7 @@ const HeaderUserContent = () => {
           <div className="flex space-x-4">
             {user.role === UserRole.USER && <Badge variant="default">user</Badge>}
             {user.role === UserRole.ADMIN && <Badge variant="success">admin</Badge>}
-            <p>{user?.name}</p>
+            <p className="overflow-hidden text-ellipsis">{user?.name}</p>
           </div>
         </div>
       </div>

beb27ba2546ffb43a8ebe6842aec06de6069cc33
mbaluev <mikhail.baluev@gmail.com>
2025-10-15 20:07:34 +0200

    error-block

--- src/components/molecules/error-block.tsx
+++ src/components/molecules/error-block.tsx
@@ -10,7 +10,7 @@ type ErrorBlockProps = ComponentProps<'div'> & ErrorBlockBaseProps;
 const ErrorBlock = forwardRef<HTMLDivElement, ErrorBlockProps>((props, ref) => {
   const { icon, code, name, ...rest } = props;
   return (
-    <div ref={ref} className="flex flex-col gap-6 items-center w-full py-10" {...rest}>
+    <div ref={ref} className="flex flex-col gap-6 items-center w-full" {...rest}>
       {icon && <p className="text-[10rem]">{icon}</p>}
       <div className="flex flex-col gap-4 items-center">
         {code && <p className="text-3xl font-medium text-center">{code}</p>}
--- src/components/organisms/widgets/illustrations.tsx
+++ src/components/organisms/widgets/illustrations.tsx
@@ -6,24 +6,38 @@ import { SvgTrash } from '@/components/icons/components/trash';
 import { SvgFiles } from '@/components/icons/components/files';
 import { SvgSpam } from '@/components/icons/components/spam';
 import { SvgSync } from '@/components/icons/components/sync';
+import { cn } from '@/lib/utils/cn';
 
 export const WidgetIllustrations = (props: WidgetProps) => {
   return (
     <Widget {...props}>
       <WidgetContent className="@container/illustrations">
-        <div className="w-full grid gap-10 px-10 grid-cols-1 @xl/illustrations:grid-cols-2 @4xl/illustrations:grid-cols-3 @7xl/illustrations:grid-cols-4">
+        <div
+          className={cn(
+            'w-full p-10 grid gap-10 grid-cols-1',
+            '@xl/illustrations:grid-cols-2 @4xl/illustrations:grid-cols-3 @7xl/illustrations:grid-cols-4'
+          )}
+        >
           <ErrorBlock
             icon={<SvgTrash />}
             code="trash"
-            name="messages that have been in trash more than 30 days will be automatically deleted"
+            name="messages that have been in trash more than 30 days will be deleted"
           />
           <ErrorBlock
             icon={<SvgFiles />}
             code="files"
             name="designed to store, organize, manage, and track your files efficiently"
           />
-          <ErrorBlock icon={<SvgSpam />} code="spam" name="..." />
-          <ErrorBlock icon={<SvgSync />} code="sync" name="..." />
+          <ErrorBlock
+            icon={<SvgSpam />}
+            code="spam"
+            name="stores unwanted, suspicious, or potentially harmful emails detected"
+          />
+          <ErrorBlock
+            icon={<SvgSync />}
+            code="sync"
+            name="ensures that data remains consistent and up to date across the universe"
+          />
         </div>
       </WidgetContent>
     </Widget>
